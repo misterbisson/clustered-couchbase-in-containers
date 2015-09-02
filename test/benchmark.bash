9a052d1a3c9934bb78b02eaea3241c0604333974
Tim Gross <tim@0x74696d.com>
2015-09-02 17:19:21 -0400

    Tune down number of threads in test client to avoid crashes

--- test/benchmark.bash
+++ test/benchmark.bash
@@ -11,11 +11,11 @@ HOSTSTRING=$(IFS="," ; echo "${HOSTS[*]}")
 
 echo Running benchmark vs $HOSTSTRING...
 
-docker run -d -m 8G 0x74696d/pillowfight cbc-pillowfight \
+docker run -d -m 4G 0x74696d/pillowfight cbc-pillowfight \
        --spec couchbase://$HOSTSTRING/benchmark \
        --min-size=800 \
        --max-size=800 \
-       --num-threads=64 \
+       --num-threads=32 \
        --num-items=1000000 \
        --batch-size=5000 \
        --num-cycles=10000 \
