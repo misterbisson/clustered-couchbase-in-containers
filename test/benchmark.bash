31e7c34f2c73b9f3cb16b42fa3a6a44cc3318fc1
Tim Gross <tim@0x74696d.com>
2015-09-03 09:09:53 -0400

    Update benchmark to use docker-compose for scaling

--- test/benchmark.bash
+++ test/benchmark.bash
@@ -1,25 +1,50 @@
 #!/bin/bash
 
-# create a list of our couchbase nodes
+# create a list of our linked couchbase nodes
 echo Querying for couchbase nodes...
 HOSTS=()
-for container in `docker ps | grep ccic_couchbase | awk -F' +' '{print $NF}'`
+for ip in `env | grep "CCIC_COUCHBASE_.*_PORT_8092_TCP_ADDR" | cut -d'=' -f2`
 do
-    HOSTS+=(`docker inspect $container | json -a NetworkSettings.IPAddress`)
+    HOSTS+=("$ip")
 done
 HOSTSTRING=$(IFS="," ; echo "${HOSTS[*]}")
+NUM_HOSTS=${#HOSTS[@]}
+
+echo Polling couchbase nodes...
+CLUSTERFOUND=0
+while [ "$CLUSTERFOUND" -lt ${NUM_HOSTS} ]; do
+    echo -n '.'
+    sleep 19
+
+    CLUSTERFOUND=$(curl -sL http://consul:8500/v1/catalog/service/couchbase | json -aH ServiceAddress | wc -l)
+done
+sleep 3
+
+CLUSTERFOUND=0
+while [ $CLUSTERFOUND != 1 ]; do
+    echo -n '.'
+
+    CLUSTERIP=$(curl -sL http://consul:8500/v1/catalog/service/couchbase | json -aH ServiceAddress | head -1)
+    if [ -n "$CLUSTERIP" ]
+    then
+        let CLUSTERFOUND=1
+    else
+        sleep 3
+    fi
+done
+sleep 3
+
 
 echo Running benchmark vs $HOSTSTRING...
-
-docker run -d -m 4G 0x74696d/pillowfight cbc-pillowfight \
-       --spec couchbase://$HOSTSTRING/benchmark \
-       --min-size=800 \
-       --max-size=800 \
-       --num-threads=32 \
-       --num-items=1000000 \
-       --batch-size=5000 \
-       --num-cycles=10000 \
-       --set-pct=10 \
-       --rate-limit=1000000 \
-       --timings \
-       --verbose
+cbc-pillowfight \
+    --spec couchbase://$HOSTSTRING/benchmark \
+    --min-size=800 \
+    --max-size=800 \
+    --num-threads=32 \
+    --num-items=1000000 \
+    --batch-size=5000 \
+    --num-cycles=-1 \
+    --set-pct=10 \
+    --rate-limit=1000000 \
+    --timings \
+    --verbose
